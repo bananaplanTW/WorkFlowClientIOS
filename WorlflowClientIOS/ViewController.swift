6c164c3299d52c7dbfdf06bbca26c976f2cd4c9c
Daz <daz.lee1987@gmail.com>
2015-12-09 17:12:24 +0800

    Initial Commit

---
+++ WorlflowClientIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorlflowClientIOS
+//
+//  Created by Daz on 12/9/15.
+//  Copyright © 2015 Daz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
