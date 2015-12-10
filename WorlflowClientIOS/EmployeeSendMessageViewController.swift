65e80f02585dc3617a3a195e20581d2c870d2964
Daz <daz.lee1987@gmail.com>
2015-12-10 15:21:33 +0800

    add send message view comtroller

---
+++ WorlflowClientIOS/EmployeeSendMessageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EmployeeSendMessageViewController.swift
+//  WorlflowClientIOS
+//
+//  Created by Daz on 12/10/15.
+//  Copyright © 2015 Daz. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeSendMessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func doneButton(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
